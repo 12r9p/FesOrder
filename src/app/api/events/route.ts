6702bd91a3ede6ec23005a9a814b055f21c3b151
12r <sato.t.5970@gmail.com>
2024-10-22 02:22:05 +0000

    イベント名とサークル名からサークルIDを返すAPI

---
+++ src/app/api/events/route.ts
@@ -0,0 +1,71 @@
+import { Client } from "@notionhq/client";
+import { NextRequest, NextResponse } from "next/server";
+import dotenv from "dotenv";
+
+// 環境変数を読み込む
+dotenv.config();
+
+const notion = new Client({ auth: process.env.NOTION_API_TOKEN });
+const NOTION_DATABASE_EVENTS = process.env.NOTION_DATABASE_EVENTS!;
+
+interface Events {
+    eventName: string;
+    circleName: string;
+    circleId: string;
+};
+
+export async function GET(req: NextRequest) {
+    const { searchParams } = new URL(req.url);
+    const eventName = searchParams.get("eventName");
+    const circleName = searchParams.get("circleName");
+
+  if (!eventName) {
+    return NextResponse.json(
+      { error: "circleId is required" },
+      { status: 400 }
+    );
+  }
+    
+    try {
+
+    const filters: any[] = [
+        {
+            property: "eventName",
+            rich_text: {
+                equals: eventName,
+            },
+        },
+    ];
+
+    if (circleName) {
+        filters.push({
+            property: "circleName",
+            rich_text: {
+                equals: circleName,
+            },
+        });
+    }
+        // イベントデータを取得
+        const eventResponse = await notion.databases.query({
+            database_id: NOTION_DATABASE_EVENTS!,
+            filter: {
+                and: filters,
+            },
+        });
+
+        const eventResults = eventResponse.results || [];
+        const events: Events[] = eventResults.map((eventPage: any) => ({
+            eventName: eventPage.properties.eventName.title?.[0]?.text?.content || "",
+            circleName: eventPage.properties.circleName.rich_text?.[0]?.text?.content || "",
+            circleId: eventPage.properties.circleId.rich_text?.[0]?.text?.content || "",
+        }));
+
+        return NextResponse.json(events);
+  } catch (error) {
+    console.error(error);
+        return NextResponse.json(
+            { error: "Error querying database" },
+            { status: 500 }
+        );
+  }
+}
